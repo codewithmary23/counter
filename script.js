8d228dde6d424319d757af171d15e7a1600ce6f2
codewithmary23 <92807654+codewithmary23@users.noreply.github.com>
2021-12-02 15:31:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Elements
+const counterDisplayElem = document.getElementById("number");
+const counterPlusElem = document.querySelector('.number-up');
+const counterMinusElem = document.querySelector('.number-down');
+const counterReset = document.querySelector('.number-reset');
+
+let count = Math.floor(Math.random() * 80);
+
+// Functions
+updateDisplay();
+
+function updateDisplay() {
+  counterDisplayElem.innerHTML = count;
+}
+
+// Event listeners
+counterPlusElem.addEventListener('click', () => {
+  count++;
+  updateDisplay();
+});
+
+counterMinusElem.addEventListener('click', () => {
+  count--;
+  updateDisplay();
+});
+
+counterReset.addEventListener('click', () => {
+  count = Math.floor(Math.random() * 80);
+  updateDisplay();
+});
